e5d6da85948f33fc96f6af960f28afd0de305283
sinux1 <>
2021-01-20 15:44:09 -0800

    Working delete button. Still concerned about the edit button

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', { 
@@ -10,7 +11,9 @@ mongoose.connect('mongodb://localhost/blog', {
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
-
+// Whenever the parameter _method is set in any type
+// of form situation, allows to override the method (for delete)
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
   // articles is a list of all articles in database, sorted in descending 
