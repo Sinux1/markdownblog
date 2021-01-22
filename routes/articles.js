2542a23755f670a2e0a60e237c9c557e980b7743
sinux1 <>
2021-01-21 17:11:00 -0800

    Save button functionality from edit screen is full operational @ 1:01:00 in vid. Need to ass edit button to index page

--- routes/articles.js
+++ routes/articles.js
@@ -24,22 +24,33 @@ router.get('/:slug', async (req, res) =>{
 
 })
 
-router.post('/', async (req, res) =>{
-  let article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  })
+router.post('/', async (req, res, next) =>{
+  req.article = new Article()
+  next()
+} , saveArticleAndRedirect('new'))
 
-  try {
-    article = await article.save()
-    res.redirect(`/articles/${article.slug}`)
-  } catch (e){
-    // Log to console the error
-    console.log(e)
-    res.render('articles/new', {article: article  })
+router.put('/:id', async (req, res, next) =>{
+  req.article = await Article.findById(req.params.id)
+  next()
+} , saveArticleAndRedirect('new'))
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article
+      article.title = req.body.title,
+      article.description = req.body.description,
+      article.markdown = req.body.markdown
+  
+    try {
+      article = await article.save()
+      res.redirect(`/articles/${article.slug}`)
+    } catch (e){
+      // Log to console the error
+      console.log(e)
+      res.render(`articles/${article.slug}`, {article: article  })
+    }
   }
-})
+}
 
 router.delete('/:id', async (req, res) => {
   await Article.findByIdAndDelete(req.params.id)
